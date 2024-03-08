2f5d41688440f7f4057f1cc72813f8f0d11555f7
严昕辰 <3190103296@zju.edu.cn>
2024-03-08 17:29:14 -0500

    fix tilting

--- bamboo_generator.py
+++ bamboo_generator.py
@@ -23,9 +23,18 @@ class Bamboo:
         angle = 360 / self.resolution
         vert_seg = 11
 
+        low_height = 0
+        low_offset = 0
+
         for z in range(0, self.segments):
-            low_height = z * self.height - z * self.height * (
-                        self.ridge_height / 2 + random.uniform(-self.ridge_height / 20, self.ridge_height / 20))
+
+            # random tilt in local segment
+            tilt =  self.tilt * random.uniform(-1, 1)
+            tiltCoord = lambda x, y, z: (low_offset + (x - low_offset) * math.cos(math.radians(tilt))
+                                         - (z - low_height) * math.sin(math.radians(tilt)) * random.uniform(0.99, 1.01), y * random.uniform(0.99, 1.01), 
+                                         low_height + (x - low_offset) * math.sin(math.radians(tilt))
+                                         + (z - low_height) * math.cos(math.radians(tilt)) * random.uniform(0.99, 1.01))
+
             waist = self.waist_size * random.uniform(0.98, 1.02)
             ridge = self.ridge_size * random.uniform(0.98, 1.02)
             radius = self.stalk_radius * random.uniform(0.98, 1.02)
@@ -33,17 +42,17 @@ class Bamboo:
             # Vertices
             for indexV in range(0, self.resolution):
                 x, y = radius * math.cos(math.radians(angle * indexV)), radius * math.sin(math.radians(angle * indexV))
-                verts.append((x, y, low_height))
-                verts.append((x, y, low_height + self.height * self.ridge_height / 4))
-                verts.append((x * waist, y * waist, low_height + self.height * self.ridge_height))
-                verts.append((x * waist ** 2, y * waist ** 2, low_height + self.height * self.ridge_height * 2))
-                verts.append((x * waist ** 2, y * waist ** 2, low_height + self.height * self.ridge_height * 3))
-                verts.append((x * waist, y * waist, low_height + self.height * self.ridge_height * 4))
-                verts.append((x, y, low_height + self.height * self.ridge_height * 7))
-                verts.append((x, y, low_height + self.height * self.ridge_height * 7.55))
-                verts.append((x * ridge, y * ridge, low_height + self.height * self.ridge_height * 7.70))
-                verts.append((x * ridge, y * ridge, low_height + self.height * self.ridge_height * 7.85))
-                verts.append((x, y, low_height + self.height * self.ridge_height * 8))
+                verts.append(tiltCoord(x + low_offset, y, low_height))
+                verts.append(tiltCoord(x + low_offset, y, low_height + self.height * self.ridge_height / 4))
+                verts.append(tiltCoord(x * waist + low_offset, y * waist, low_height + self.height * self.ridge_height))
+                verts.append(tiltCoord(x * waist ** 2 + low_offset, y * waist ** 2, low_height + self.height * self.ridge_height * 2))
+                verts.append(tiltCoord(x * waist ** 2 + low_offset, y * waist ** 2, low_height + self.height * self.ridge_height * 3))
+                verts.append(tiltCoord(x * waist + low_offset, y * waist, low_height + self.height * self.ridge_height * 4))
+                verts.append(tiltCoord(x + low_offset, y, low_height + self.height * self.ridge_height * 7))
+                verts.append(tiltCoord(x + low_offset, y, low_height + self.height * self.ridge_height * 7.55))
+                verts.append(tiltCoord(x * ridge + low_offset, y * ridge, low_height + self.height * self.ridge_height * 7.70))
+                verts.append(tiltCoord(x * ridge + low_offset, y * ridge, low_height + self.height * self.ridge_height * 7.85))
+                verts.append(tiltCoord(x + low_offset, y, low_height + self.height * self.ridge_height * 8))
 
             # Faces
             for indexF in range(0, self.resolution - 1):
@@ -62,16 +71,7 @@ class Bamboo:
                 face = (a, b, c, d)
                 faces.append(face)
 
-        # Tilt
-        tilt = []
-        for h in range(0, self.segments):
-            tilt.append(self.tilt * random.uniform(0.99, 1.01))
-        for i, vert in enumerate(verts):
-            x, y, z = vert
-            seg = i // (vert_seg * self.resolution)
-            x = x * math.cos(math.radians(tilt[seg])) - z * math.sin(math.radians(tilt[seg]))
-            z = x * math.sin(math.radians(tilt[seg])) + z * math.cos(math.radians(tilt[seg]))
-            verts[i] = (x, y, z)
+            low_offset, _, low_height = tiltCoord(low_offset, 0, low_height + self.height * (1 - self.ridge_height / 2))
 
         self.verts = verts
         self.faces = faces
@@ -153,9 +153,9 @@ def register_properties():
     bpy.types.Scene.bamboo_resolution = bpy.props.IntProperty(name="Resolution", default=32, min=16, max=64)
     bpy.types.Scene.bamboo_stalk_radius = bpy.props.FloatProperty(name="Stalk Radius", default=1, min=0.2, max=4.0)
     bpy.types.Scene.bamboo_ridge_size = bpy.props.FloatProperty(name="Ridge Size", default=1.03, min=1.01, max=1.1)
-    bpy.types.Scene.bamboo_waist_size = bpy.props.FloatProperty(name="Waist Size", default=0.95, min=0.8, max=0.99)
+    bpy.types.Scene.bamboo_waist_size = bpy.props.FloatProperty(name="Waist Size", default=0.95, min=0.90, max=0.99)
     bpy.types.Scene.bamboo_height = bpy.props.FloatProperty(name="Height", default=5.0, min=1, max=10.0)
-    bpy.types.Scene.bamboo_tilt = bpy.props.FloatProperty(name="Tilt", default=1.0, min=0.0, max=3.0)
+    bpy.types.Scene.bamboo_tilt = bpy.props.FloatProperty(name="Tilt", default=1.0, min=0.0, max=10.0)
 
 
 classes = [BambooGenerator, BambooGeneratorPanel]
